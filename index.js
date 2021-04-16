fbf452f6765307ae389b5079b3a5eb6cfed6a0f8
Manuel Villasenor <manuelvillasenor@2@gamil.com>
2021-04-15 19:43:19 -0700

    fixed

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//make 'require' variables
+//make prompt with questions
+  //make sure to use corect input type
+//console log responses so that you can target elements on th object created
+//make a var that has a string litterall of the entire readme, and add the inputs to it
+//do a fs writefile so that a new file is created
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-//needs if statements for license badge
-//needs clickable table of contents that takes you to certain location in readme
 
 
 inquirer.prompt([
@@ -17,7 +21,7 @@ inquirer.prompt([
         type: 'list',
         message: 'What license badge would you like?',
         name: 'license',
-        choices:[ 'MIT License', 'Mozilla License', ]
+        choices:[ 'MIT', 'Mozilla', 'Apache','AGPL','GPLv3' ]
       },
      
       {
@@ -68,47 +72,66 @@ inquirer.prompt([
 .then ((response) => {
     console.log(response);
 
-    var title = response.title
-    var license = response.license
-    var description = response.description
-    var usage = response.usage
-    var contribution = response.contribution
-    var test = response.test
-    var email = response.email
-    var github = response.github
+    var title = response.title;
+    var license = response.license;
+    var description = response.description;
+    var install = response.install;
+    var usage = response.usage;
+    var contribution = response.contribution;
+    var test = response.test;
+    var email = response.email;
+    var github = response.github;
 
     var readmeFile = 
     `# ${title}
    
-${license}
+![License: ${license}](https://img.shields.io/badge/License-${license}-orange.svg)
 
-## Description
- 
+## Table of Contents
+
+1. [Description](#description)
+2. [How To Install](#install)
+3. [How To Use](#use)
+4. [How To Constribute](#contribute)
+5. [How To Test](#test)
+6. [Github/Contact Info](#questions)
+
+## Description 
+<h2 id="description"></h2> 
+
 ${description}
 
+## How To Install
+<h2 id="install"></h2> 
+
+${install}
+
 ## How To Use
+<h2 id="use"></h2> 
 
 ${usage}
     
 ## How To Constribute
+<h2 id="contribute"></h2> 
 
 ${contribution}
 
 ## How To Test
+<h2 id="test"></h2> 
 
 ${test} 
 
 ## Questions
-    
-What is my github?
-
-https://github.com/${github}
+<h2 id="questions"></h2> 
 
 How do you contact me?
 
-${email}
+feel free to reach me at ${email}
 
-    `
+Where can i view other projects?
+
+Checkout my [Github](https://github.com/${github}) for other repositories!
+  `
     
 
 
